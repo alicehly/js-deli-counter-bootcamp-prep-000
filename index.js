617bab26103621f8cfa1ffc6271c44e1b8722dee
Alice Ly Ms <alicehly@gmail.com>
2019-05-16 20:25:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ return `Welcome. You are number ${katzDeliLine.length} in line.`
 }
 
 
-
-
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     var nowServing = katzDeliLine[0];
