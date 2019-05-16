52d51c871d0760306ff3650473cad739ce5627da
Alice Ly Ms <alicehly@gmail.com>
2019-05-16 20:15:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine){
   // katzDeliLine.push(name)
   // return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+  
 katzDeliLine.push()
  
 
